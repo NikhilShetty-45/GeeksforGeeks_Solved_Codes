9d53f319df3a83e8912e24e621f2a53aa3be682a
NIKHIL <116481885+NikhilShetty-45@users.noreply.github.com>
2023-06-18 18:58:18 +0530

    Update Maximum of all subarrays of size k.java

--- Maximum of all subarrays of size k.java
+++ Maximum of all subarrays of size k.java
@@ -1,6 +1,5 @@
 class Solution
 {
-    //Function to find maximum of each subarray of size k.
     static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
     {
         ArrayList<Integer> list = new ArrayList<>();
